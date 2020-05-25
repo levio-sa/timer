74eb007cc55857e88dc8aff66c81a8d00e041ac1
Sarthak <sarthak44singla@gmail.com>
2020-05-25 19:15:58 +0530

    Modified Templates.

--- timer/models.py
+++ timer/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
+import datetime
+from datetime import date
 
 # Create your models here.
 
 class Event(models.Model):
     name = models.CharField(max_length=100)
     time=models.DateField()
-    time_until_event=models.DateField(auto_now=True)
-    published_date=models.DateField()
+    #time_until_event=time.-datetime.date.today()
+    published_date=models.DateField(blank=True, null=True)
     
     def publish(self):
-        
+        published_date=datetime.date.today()
         self.save()
     def __str__(self):
         return self.name
--- timer/views.py
+++ timer/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Event
+import datetime
+from datetime import date
 
 # Create your views here.
 
 def event_list(request):
-    return render(request, 'timer/event_list.html',{})
+    events=Event.objects.order_by('time')
+    for event in events:
+        event.time_until_event=(event.time-datetime.date.today()).days
+    return render(request, 'timer/event_list.html',{'events':events})
