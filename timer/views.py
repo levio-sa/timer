3cb3d44f00016cda3f2db9a059eae60b1e6b8f9e
Sarthak <sarthak44singla@gmail.com>
2020-05-26 19:18:23 +0530

    Added event detail and CSS and used Bootstrap.

--- timer/views.py
+++ timer/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Event
 import datetime
 from datetime import date
@@ -10,3 +10,9 @@ def event_list(request):
     for event in events:
         event.time_until_event=(event.time-datetime.date.today()).days
     return render(request, 'timer/event_list.html',{'events':events})
+
+def event_detail(request, pk):
+    
+    event=get_object_or_404(Event, pk=pk)
+    event.time_until_event=(event.time-datetime.date.today()).days
+    return render(request, 'timer/event_detail.html', {'event':event})
